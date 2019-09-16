cf6632ac8a4b7184cc6a673ee70bb936fcb89bb6
Legendante <jacques@legendante.com>
2019-09-16 15:43:48 +0200

    Clean conf file

---
+++ serverconf.inc.php
@@ -0,0 +1,15 @@
+<?php
+$dbHost = 'localhost';
+$dbUser = 'user';
+$dbPass = 'password';
+$dbName = 'dbname';
+
+$mailhost = 'mailhost';
+$mailport = '587';
+$mailusername = 'mail@domain.com';
+$mailpassword = 'passwordhere';
+define('PASSTHEPEPPER', 'PEPPERSTRINGPEPPERSTRINGPEPPERSTRINGPEPPERSTRINGPEPPERSTRING');
+define('FILEKEY', 'FILEKEYFILEKEYFILEKEYFILEKEYFILEKEYFILEKEYFILEKEYFILEKEY');
+define('ENCRYPT_ALGO', 'AES-256-CBC');
+define('ENC_IV', 'ENCIVENCIVENCIVENCIVENCIVENCIVENCIVENCIV');
+?>
